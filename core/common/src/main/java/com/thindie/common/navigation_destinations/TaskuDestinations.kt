644bd8f6dda0d4397a7c3b9439207fb3bcbe211c
Alex <thindie1@mail.ru>
2024-02-19 00:21:25 +0200

    common: refactor destinations

--- core/common/src/main/java/com/thindie/common/navigation_destinations/TaskuDestinations.kt
+++ core/common/src/main/java/com/thindie/common/navigation_destinations/TaskuDestinations.kt
@@ -1,12 +1,7 @@
 package com.thindie.common.navigation_destinations
 
 object TaskuDestinations {
-    const val priceInfo = "priceInfo"
-    const val contacts = "contacts"
-    const val eventCalendar = "eventCalendar"
-    const val excursions = "excursions"
-    const val news = "news"
-    const val main = "main"
-    const val tasksGeneral = "tasksGeneral"
-    const val userTickets = "tickets"
+
+    const val unsortedTasks = "unsortedTasks"
+    const val sortedTasksArea = "sortedTasksArea"
 }
