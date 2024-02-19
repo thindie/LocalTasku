b2ea31f5a9ed9222dc757ea02fadf6c572f946be
Alex <thindie1@mail.ru>
2024-02-20 00:53:12 +0200

    common: refactor tasku destinations

--- core/common/src/main/java/com/thindie/common/navigation_destinations/TaskuDestinations.kt
+++ core/common/src/main/java/com/thindie/common/navigation_destinations/TaskuDestinations.kt
@@ -2,6 +2,6 @@ package com.thindie.common.navigation_destinations
 
 object TaskuDestinations {
 
-    const val unsortedTasks = "unsortedTasks"
-    const val sortedTasksArea = "sortedTasksArea"
+   const val tasksRoute = "tasksRoute"
+
 }
