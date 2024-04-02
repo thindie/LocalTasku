ed9216b689d6e29a839753da1fea29e8e5c95c47
Alex <thindie1@mail.ru>
2024-04-02 15:13:15 +0200

    feature-costs: seed Events and Viewmodel

--- features/tasks-costs/src/main/java/com/thindie/tasks_costs/presentation/TasksCostViewModel.kt
+++ features/tasks-costs/src/main/java/com/thindie/tasks_costs/presentation/TasksCostViewModel.kt
@@ -1,7 +1,19 @@
 package com.thindie.tasks_costs.presentation
 
 import androidx.lifecycle.ViewModel
+import com.thindie.tasks_costs.domain.TasksCostRepository
+import com.thindie.tasks_costs.presentation.viewmodelevent.TaskCostsViewModelEvent
 import javax.inject.Inject
 
-internal class TasksCostViewModel @Inject constructor(): ViewModel() {
+internal class TasksCostViewModel @Inject constructor(private val tasksCostRepository: TasksCostRepository) :
+    ViewModel() {
+
+    fun onEvent(taskCostsViewModelEvent: TaskCostsViewModelEvent) {
+        when (taskCostsViewModelEvent) {
+            TaskCostsViewModelEvent.OnSelectDay -> tasksCostRepository
+            TaskCostsViewModelEvent.OnSelectMonth -> TODO()
+            TaskCostsViewModelEvent.OnSelectWeek -> TODO()
+            TaskCostsViewModelEvent.OnSelectYear -> TODO()
+        }
+    }
 }
